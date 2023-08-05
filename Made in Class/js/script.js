23bf1d82dd52c7b3f35de2072318b4dc3c770a7a
Gabriel Carrascosa <141431095+Batatexy@users.noreply.github.com>
2023-08-05 14:07:10 -0300

    Add files via upload

---
+++ Made in Class/js/script.js
@@ -0,0 +1,24 @@
+function alemanhapag() {
+    window.location.href = "alemanha.html"
+}
+function argentinapag() {
+    window.location.href = "argentina.html"
+}
+function brasilpag() {
+    window.location.href = "brasil.html"
+}
+function françapag() {
+    window.location.href = "frança.html"
+}
+function inglaterrapag() {
+    window.location.href = "inglaterra.html"
+}
+function jamaicapag() {
+    window.location.href = "jamaica.html"
+}
+function suicapag() {
+    window.location.href = "suica.html"
+}
+function limpar() {
+    window.location.href = "../index.html"
+}
